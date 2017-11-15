023ac8be53c84131e8e579cebeb1e9fecff88ee8
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-15 17:52:19 -0200

    Creating Playlist and Thumbnail Entities

---
+++ ViperYouTube/Entities/Playlist.swift
@@ -0,0 +1,16 @@
+//
+//  Playlist.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 15/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+import Foundation
+
+struct Playlist {
+    var id: String
+    var title: String
+    var thumbnail: Thumbnail
+}
+
---
+++ ViperYouTube/Entities/Thumbnail.swift
@@ -0,0 +1,14 @@
+//
+//  Thumbnail.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 15/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+import Foundation
+
+struct Thumbnail {
+    var url: String
+}
+
