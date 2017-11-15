005a45d0c1a5a0d4f9bf7c3a9b59bc62da97095e
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-15 17:12:20 -0200

    Creating endpoints for playlists

--- ViperYouTube/Network/Endpoints.swift
+++ ViperYouTube/Network/Endpoints.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct API {
+    static let baseURL = "https://www.googleapis.com/youtube/v3/"
+    static let key = "AIzaSyBTbjUFwC5C29tnu9MJZm5kaWZ4hCKqgz0"
+}
+
+protocol Endpoint {
+    var path: String { get }
+    var url: String { get }
+    var parameters: String { get }
+}
+
+enum Endpoint {
+    
+    enum Playlists: Endpoint {
+        case fetch(part: String, channelID: String)
+        
+        public var path: String {
+            switch self {
+            case .fetch:
+                return "playlists"
+            }
+        }
+        
+        public var url: String {
+            switch self {
+            case .fetch:
+                return "\(API.baseUrl)\(path)\(parameters)"
+            }
+        }
+        
+        public var parameters: String {
+            switch self {
+            case .fetch(let part, let channelID):
+                return "?part=\(part)&channelId=\(channelID)&key=\(API.key)"
+            }
+        }
+    }
+    
+}
