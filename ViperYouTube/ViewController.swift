62ebdd366b95d18e958f5c34d3085028c9078e6e
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-15 16:36:49 -0200

    Initial Commit

---
+++ ViperYouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 15/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
