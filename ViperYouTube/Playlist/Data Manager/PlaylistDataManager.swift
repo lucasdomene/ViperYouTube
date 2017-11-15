1ce698d5977ba2710f446e0cbf9c5912cb756775
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-15 16:49:37 -0200

    Creating PlaylistDataManager

---
+++ ViperYouTube/Playlist/Data Manager/PlaylistDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  PlaylistDataManager.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 15/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+import Foundation
