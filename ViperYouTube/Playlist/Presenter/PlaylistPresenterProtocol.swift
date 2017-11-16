38f4f19c33ef03dee8a764622cc44e8c421a1d64
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-16 21:32:11 -0200

    Adding Presenter Protocol

---
+++ ViperYouTube/Playlist/Presenter/PlaylistPresenterProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  PlaylistPresenterProtocol.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 16/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+import Foundation
+
+protocol PlaylistPresenterProtocol: class {
+    var view: PlaylistViewProtocol? { get set }
+    
+    func updateView()
+    func showDetailsForPlaylist(_ playlist: Playlist)
+}
