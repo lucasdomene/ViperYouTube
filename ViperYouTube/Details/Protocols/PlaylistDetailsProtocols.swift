0d64b2c971bc463eed6d3d3917398364018abd2f
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-17 00:29:36 -0200

    Created Presenter and View details protocols

--- ViperYouTube/Details/Protocols/PlaylistDetailsProtocols.swift
+++ ViperYouTube/Details/Protocols/PlaylistDetailsProtocols.swift
@@ -6,22 +6,49 @@
 //  Copyright © 2017 Luca Domene Firmo. All rights reserved.
 //
 
-protocol DetailsDataManagerInputProtocol: class {
-    var dataManagerOutput: DetailsDataManagerOutputProtocol? { get set }
+// MARK: - Data Manager Protocols
+
+protocol PlaylistDetailsDataManagerInputProtocol: class {
+    var dataManagerOutput: PlaylistDetailsDataManagerOutputProtocol? { get set }
     func fetchVideos(forPlaylist: Playlist)
 }
 
-protocol DetailsDataManagerOutputProtocol: class {
+protocol PlaylistDetailsDataManagerOutputProtocol: class {
     func onVideosFetched(videos: [Video])
     func onError()
 }
 
-protocol DetailsInteractorInputProtocol: class {
-    var interactorOutput: DetailsInteractorOutputProtocol { get set }
+// MARK: - Interactor Protocols
+
+protocol PlaylistDetailsInteractorInputProtocol: class {
+    var interactorOutput: PlaylistDetailsInteractorOutputProtocol? { get set }
     func fetchVideos()
 }
 
-protocol DetailsInteractorOutputProtocol: class {
+protocol PlaylistDetailsInteractorOutputProtocol: class {
     func videosFetched(videos: [Video])
     func onError()
 }
+
+// MARK: - Presenter Protocols
+
+protocol PlaylistDetailsPresenterProtocol: class {
+    //var view
+    var interactor: PlaylistDetailsInteractorOutputProtocol? { get set }
+    // var router
+    
+    func viewDidLoad()
+    func showDetailsForVideo(_ video: Video)
+}
+
+// MARK: - View Protocols
+
+protocol PlaylistDetailsViewProtocol: class {
+    var presenter: PlaylistDetailsPresenterProtocol? { get set }
+    func showVideos(_ videos: [Video])
+    func showError()
+    func showLoading()
+    func hideLoading()
+}
+
+// MARK: - Router Protocols
