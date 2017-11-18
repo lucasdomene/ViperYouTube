40289362dd150079a7ad80fd31b3cf51b511a4b7
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-18 00:21:11 -0200

    Implementing Video Interactor

---
+++ ViperYouTube/VideoDetails/Interactor/VideoInteractor.swift
@@ -0,0 +1,30 @@
+//
+//  VideoInteractor.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 18/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+class VideoInteractor: VideoInteractorInputProtocol {
+    
+    var interactorOutput: VideoInteractorOutputProtocol?
+    var dataManager: VideoDataManagerInputProtocol?
+    
+    func fetchVideoDetails(forVideo video: Video) {
+        dataManager?.fetchVideoDetails(forVideo: video)
+    }
+    
+}
+
+extension VideoInteractor: VideoDataManagerOutputProtocol {
+    
+    func onVideoDetailsFetched(video: Video) {
+        interactorOutput?.videoDetailsFetched(video: video)
+    }
+    
+    func onError() {
+        interactorOutput?.onError()
+    }
+    
+}
