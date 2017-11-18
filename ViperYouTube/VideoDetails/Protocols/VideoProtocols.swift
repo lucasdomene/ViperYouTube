e8c55db3b6e5c0e9206888e10ac4ec6743f0b6d7
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-17 23:48:09 -0200

    Declaring protocols for video

--- ViperYouTube/VideoDetails/Protocols/VideoProtocols.swift
+++ ViperYouTube/VideoDetails/Protocols/VideoProtocols.swift
@@ -6,14 +6,21 @@
 //  Copyright © 2017 Luca Domene Firmo. All rights reserved.
 //
 
+import UIKit
+
 protocol VideoViewProtocol: class {
-    
+    var presenter: VideoPresenterProtocol? { get set }
+    func showVideoDetails(_ video: Video)
 }
 
 protocol VideoWireFrameProtocol: class {
-    
+    static func createVideoDetailsViewController(forVideo video: Video) -> UIViewController
 }
 
 protocol VideoPresenterProtocol: class {
+    var view: VideoViewProtocol? { get set }
+    var wireFrame: VideoWireFrameProtocol? { get set }
+    var video: Video? { get set }
     
+    func viewDidLoad()
 }
