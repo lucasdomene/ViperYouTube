13f791d26b15944a526766cc79eef4146f97bc69
Lucas Firmo <lucas.firmo@gmail.com>
2017-11-17 23:45:46 -0200

    Creating video protocols file

---
+++ ViperYouTube/VideoDetails/Protocols/VideoProtocols.swift
@@ -0,0 +1,19 @@
+//
+//  VideoProtocols.swift
+//  ViperYouTube
+//
+//  Created by Lucas Domene Firmo on 17/11/17.
+//  Copyright © 2017 Luca Domene Firmo. All rights reserved.
+//
+
+protocol VideoViewProtocol: class {
+    
+}
+
+protocol VideoWireFrameProtocol: class {
+    
+}
+
+protocol VideoPresenterProtocol: class {
+    
+}
